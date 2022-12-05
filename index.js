8b38f1cd3fdf110e570225aec263f79315d10f24
gonzalogaete <gonzalogaete110@gmail.com>
2022-12-05 19:23:09 -0300

    a

---
+++ index.js
@@ -0,0 +1,20 @@
+const server = require("./src/app.js");
+const { conn } = require("./src/db.js");
+const createAllProducts = require("./src/dbControllers/index");
+
+// Syncing all the models at once.
+
+async function main() {
+  try {
+    conn.sync({ force: true }).then(async () => {
+      await createAllProducts();
+      server.listen(process.env.PORT || 3001, async () => {
+        console.log("%s listening at 3001"); // eslint-disable-line no-console
+      });
+    });
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+}
+
+main();
---
+++ src/dbControllers/index.js
@@ -0,0 +1,12 @@
+const pcData = require("../data/pcData.json");
+const { Producto, User } = require("../db.js");
+const funcion = require("../../fn");
+
+function createAllProducts() {
+  const a = funcion(pcData);
+  Producto.bulkCreate(a)
+    .then(() => console.log("data created on db"))
+    .catch((error) => console.log(error));
+}
+
+module.exports = createAllProducts;
