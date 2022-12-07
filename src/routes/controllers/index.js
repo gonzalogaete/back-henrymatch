2db73ede6c111018dc312238c859ac2cace66c74
Bryan Ramos Vargas <matematica_bryan@hotmail.com>
2022-12-06 19:08:17 -0500

    route create and get users

--- src/routes/controllers/index.js
+++ src/routes/controllers/index.js
@@ -1,5 +1,6 @@
 const { expressjwt: jwt } = require("express-jwt");
 const jwks = require("jwks-rsa");
+const { User } = require("../../db/db");
 
 module.exports = {
   jwtCheck: jwt({
@@ -13,4 +14,24 @@ module.exports = {
     issuer: "https://barv.us.auth0.com/",
     algorithms: ["RS256"],
   }).unless({ path: ["/"] }),
+  getAllUsers: async (req, res) => {
+    try {
+      const users = await User.findAll();
+      res.json(users)
+    } catch (error) {
+      console.log(error)
+    }
+  },
+  saveUser: async (req, res) => {
+    try {
+      // const {} = req.body
+      console.log(req.body);
+      const [user, created] = await User.findOrCreate({
+        where: { ...req.body },
+      });
+      res.json(req.user);
+    } catch (error) {
+      console.log(error);
+    }
+  },
 };
--- src/routes/userRoute/user.js
+++ src/routes/userRoute/user.js
@@ -1,15 +1,16 @@
 const { Router } = require("express");
 const { getUser, getInterest } = require("../middlewares/index");
+const { saveUser, getAllUsers } = require("./../controllers");
 const axios = require("axios").default;
 
 const router = Router();
 
-router.get("/save", getUser, async (req, res) => {
-  res.send(req.user);
-});
+router.get("/users", getAllUsers);
 
-router.get("/interest", getInterest, async(req,res)=>{
-  res.send(req.interest)
-})
+router.get("/save", getUser, saveUser);
+
+router.get("/interest", getInterest, async (req, res) => {
+  res.send(req.interest);
+});
 
 module.exports = router;
